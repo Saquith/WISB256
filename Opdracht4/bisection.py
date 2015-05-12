1eb3ca4569e629cb28c31c8c94694a9ab65301d2
rokr <rokr@documentaal.nl>
2015-05-12 09:42:41 +0200

    Add comments, add checking if f(a) or f(b) is already 0

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,8 +1,17 @@
 def findRoot(f,a,b,epsilon) :
     m = (a + b) / 2
     
+    # Stopping criterion
     if abs(b - a) <= epsilon :
         return m
+
+    # Check if this is already a root
+    if f(a) == 0 :
+        return a
+    if f(b) == 0 :
+        return b
+
+    # Go into recursion
     if (f(a) < 0 and f(m) > 0) or (f(a) > 0 and f(m) < 0) :
         return findRoot(f,a,m,epsilon)
     if (f(m) < 0 and f(b) > 0) or (f(m) > 0 and f(b) < 0) :
@@ -15,8 +24,14 @@ def findAllRoots(f,a,b,epsilon) :
     if abs(b - a) <= epsilon :
         return [m]
 
-    # Build the lists recursively
     l = []
+    # Check if this is already a root
+    if f(a) == 0 :
+        l.append(a)
+    if f(b) == 0 :
+        l.append(b)
+        
+    # Build the lists recursively
     if (f(a) < 0 and f(m) > 0) or (f(a) > 0 and f(m) < 0) :
         l.extend(findAllRoots(f,a,m,epsilon))
     if (f(m) < 0 and f(b) > 0) or (f(m) > 0 and f(b) < 0) :
