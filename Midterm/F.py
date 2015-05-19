8f9b148e007c819fd52ddd7a9ab6ca78fff7a5ce
rokr <rokr@documentaal.nl>
2015-05-19 12:50:25 +0200

    Add Reverse Polish Notation omschrijven

---
+++ Midterm/F.py
@@ -0,0 +1,25 @@
+def isOperator(s) :
+    return s == '+' or s == '-' or s == '*' or s == '/'
+
+def calc(op, x, y) :
+    return ('(%s %s %s)' % (x, op, y))
+
+lines = input().split()
+
+while len(lines) > 1 :
+    newLines = []
+    for i in range(0, len(lines)) :
+        if len(lines) == 3 :
+            newLines = [calc(lines[2], lines[0], lines[1])]
+            break
+        elif i + 2 < len(lines) and isOperator(lines[i + 2]) and not isOperator(lines[i + 1]) and not isOperator(lines[i]) :
+            newLines.append(calc(lines[i + 2], lines[i], lines[i + 1]))
+            i += 3
+            for j in range(i, len(lines)) :
+                newLines.append(lines[j])
+            break
+        else :
+            newLines.append(lines[i])
+    lines = newLines
+
+print(lines[0])
